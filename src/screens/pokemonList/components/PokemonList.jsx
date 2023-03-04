f1414cbd6824b75aaa040a0f7c37a539875a93e6
Juan Leo <juanleodev@gmail.com>
2023-03-04 17:01:29 +0100

    Added responsiveness to grid

--- src/screens/pokemonList/components/PokemonList.jsx
+++ src/screens/pokemonList/components/PokemonList.jsx
@@ -3,7 +3,7 @@ import { PokemonListItem } from "./PokemonListItem"
 
 export function PokemonList({ items }) {
     return (
-        <SimpleGrid spacing={4} columns={2}>
+        <SimpleGrid spacing={4} columns={{ base: 3, md: 2, sm: 1}}>
             {items.map(item => <PokemonListItem key={item.name} item={item} />)}
         </SimpleGrid>
     )
